bd2e453cd758b912dfdf07fa037e1ff7ba74f180
IB-315168 <315168@via.dk>
2022-10-17 19:07:50 +0200

    User Model

---
+++ Domain/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class User
+    {
+        public ulong Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public Dictionary<string, Tuple<DateTime, DateTime>> RecurAvailablity;
+    }
+}
