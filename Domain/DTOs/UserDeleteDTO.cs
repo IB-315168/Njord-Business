ac5d68860249e64688bbecb178096801e6f059cd
Jakub babica <315190@via.dk>
2022-10-18 12:42:36 +0200

    User Delete DTO

---
+++ Domain/DTOs/UserDeleteDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.DTOs;
+
+public class UserDeleteDTO
+{
+    public ulong Id { get; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
