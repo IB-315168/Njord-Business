9137dca80f760e640bea9f72bfcd6dd53f5ae404
Jakub babica <315190@via.dk>
2022-10-18 13:10:27 +0200

    final commit

--- Data/DAOs/UserDAO.cs
+++ Data/DAOs/UserDAO.cs
@@ -37,7 +37,7 @@ public class UserDAO : IUserDAO
         return Task.FromResult(existing);
     }
 
-    public async Task DeleteAsync(User user) //async
+    public async Task DeleteAsync(User user) // async
     {
         User? existing = context.Users.FirstOrDefault(u => u.Id == user.Id);
         context.Users.Remove(existing);
