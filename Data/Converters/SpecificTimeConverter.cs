1f414f8c46447554c865e9fc85f20bb238d27d71
IB-315168 <315168@via.dk>
2022-11-21 10:37:08 +0100

    SpecificTimeConverter + ProjectService.proto build

---
+++ Data/Converters/SpecificTimeConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrpcNjordClient;
+
+namespace Data.Converters
+{
+    public class SpecificTimeConverter
+    {
+        public static DateTime convertToDateTime(SpecificTime specificTime)
+        {
+            return new DateTime(specificTime.Year, specificTime.Month, 
+                specificTime.Day, specificTime.Hour, specificTime.Minute, 0);
+        }
+
+        public static SpecificTime convertToSpecificTime(DateTime dateTime)
+        {
+            return new SpecificTime()
+            {
+                Year = dateTime.Year,
+                Month = dateTime.Month,
+                Day = dateTime.Day,
+                Hour = dateTime.Hour,
+                Minute = dateTime.Minute
+            };
+        }
+    }
+}
